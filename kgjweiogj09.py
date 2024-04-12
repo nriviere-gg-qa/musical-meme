9b97e8ec8f890019c1d9d64dcd264116f867c081
nriviere-gg-qa <nriviere-gg-qa@proton.me>
2024-04-12 12:03:41 +0200

    Update kgjweiogj09.py

--- kgjweiogj09.py
+++ kgjweiogj09.py
@@ -1,1 +1,2 @@
-token = "fw8e64fw65e4g35e1ghd32hft5"
+subdomain = 'klj3xayd3fx_ic72e2phu97w2vhj.fx-6499ztyzkx4ku9to33q818kfvtd_giewfg1ww1k.1aekuvnjngub1ap4kp.myshopify.com'
+token = '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'
